dc1a7d2ebdafa3797f1128b07bdc1a120fbecc51
bppwd-valley <36992134+bppwd-valley@users.noreply.github.com>
2018-04-11 11:09:31 -0400

    as

--- testing.py
+++ testing.py
@@ -2,6 +2,6 @@ import numpy
 import cv2
 from naoqi import ALProxy
 mo = ALProxy("ALRobotPosture","10.45.76.171",9559)
-mo.goToPosture("Stand",1)
+mo.goToPosture("Crouch",1)
 tts = ALProxy("ALTextToSpeech","10.45.76.171",9559)
 tts.say("Testing Connection")
