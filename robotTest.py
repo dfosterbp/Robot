809174f137c08a94431c5a01604442562745c72c
bppwd-valley <36992134+bppwd-valley@users.noreply.github.com>
2018-04-11 11:10:45 -0400

    no comment

--- robotTest.py
+++ robotTest.py
@@ -9,16 +9,11 @@ boundaries = [
 	([60, 20, 20], [100, 60, 60])
 ]
 
-#loop over the boundaries
 for (lower, upper) in boundaries:
-    #red for stopSign.jpg
     lower = np.array(lower, dtype = "uint8")
     upper = np.array(upper, dtype = "uint8")
-# find the colors within the specified boundaries and apply
-# the mask
 mask = cv2.inRange(img, lower, upper)
 output = cv2.bitwise_and(img, img, mask = mask)
 
-# show the images
 cv2.imshow("images", np.hstack([img, output]))
 cv2.waitKey(0)
