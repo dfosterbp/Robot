296d7eb8c50a191377b769171729e3a7b646da35
bppwd-perry <38288721+bppwd-perry@users.noreply.github.com>
2018-04-11 10:35:42 -0400

    a

--- robotTest.py
+++ robotTest.py
@@ -8,7 +8,7 @@ import cv2
 #ap.add_argument("-i", "--image", help = "path to the image")
 #args = vars(ap.parse_args())
 
-img = cv2.imread('stopSign.jpg')
+img = cv2.imread('binder.jpg')
 '''
 boundaries = [
 	([104, 97, 198], [50, 56, 200]),
@@ -22,16 +22,16 @@ for (lower, upper) in boundaries:
 	# create NumPy arrays from the boundaries
 	'''
 #red for stopSign.jpg
+'''
 lower = np.array([0, 0, 124], dtype = "uint8")
 upper = np.array([144, 145, 240], dtype = "uint8")
 #green for stopSign.jpg
-'''
+
 lower = np.array([124, 145, 44], dtype = "uint8")
 upper = np.array([149, 195, 75], dtype = "uint8")'''
 #WIP binder don't use yet
-'''
-lower = np.array([124, 145, 44], dtype = "uint8")
-upper = np.array([149, 195, 75], dtype = "uint8")'''
+lower = np.array([66, 36, 36], dtype = "uint8")
+upper = np.array([78, 57, 58], dtype = "uint8")
 
 # find the colors within the specified boundaries and apply
 # the mask
