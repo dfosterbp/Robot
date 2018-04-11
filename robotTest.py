e077a497e12a3b698f2797c7e69f83830d86e2d6
bppwd-perry <38288721+bppwd-perry@users.noreply.github.com>
2018-04-11 10:13:59 -0400

    comment

---
+++ robotTest.py
@@ -0,0 +1,53 @@
+#from naoqi import ALProxy
+
+import numpy as np
+import argparse
+import cv2
+
+#ap = argparse.ArgumentParser()
+#ap.add_argument("-i", "--image", help = "path to the image")
+#args = vars(ap.parse_args())
+
+img = cv2.imread('stopSign.jpg')
+'''
+boundaries = [
+	([104, 97, 198], [50, 56, 200]),
+	([86, 31, 4], [220, 88, 50]),
+	([25, 146, 190], [62, 174, 250]),
+	([103, 86, 65], [145, 133, 128])
+]
+
+# loop over the boundaries
+for (lower, upper) in boundaries:
+	# create NumPy arrays from the boundaries
+	'''
+#red for stopSign.jpg
+lower = np.array([0, 0, 124], dtype = "uint8")
+upper = np.array([144, 145, 240], dtype = "uint8")
+#green for stopSign.jpg
+'''
+lower = np.array([124, 145, 44], dtype = "uint8")
+upper = np.array([149, 195, 75], dtype = "uint8")'''
+#WIP binder don't use yet
+'''
+lower = np.array([124, 145, 44], dtype = "uint8")
+upper = np.array([149, 195, 75], dtype = "uint8")'''
+
+# find the colors within the specified boundaries and apply
+# the mask
+mask = cv2.inRange(img, lower, upper)
+output = cv2.bitwise_and(img, img, mask = mask)
+
+# show the images
+cv2.imshow("images", np.hstack([img, output]))
+cv2.waitKey(0)
+
+#cv2.imshow('image',img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+
+#tts = ALProxy("ALTextToSpeech", "172.17.21.168", 9559)
+#pose = ALProxy("ALRobotPosture", "172.17.21.168", 9559)
+#motion = ALProxy("ALMotion", "172.17.21.168", 9559)
+#pose.goToPosture("Stand",1)
+#motion.moveTo(0.5, 0, 0)
