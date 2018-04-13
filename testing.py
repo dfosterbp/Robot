2f02963a45291182e36486c3274250137a63587a
dfosterbp <36169496+dfosterbp@users.noreply.github.com>
2018-04-13 12:15:34 -0400

    a

--- testing.py
+++ testing.py
@@ -1,3 +1,34 @@
-import sys
 import numpy as np
 import cv2
+
+
+# Setup SimpleBlobDetector parameters.
+params = cv2.SimpleBlobDetector_Params()
+ 
+# Change thresholds
+params.minThreshold = 10;
+params.maxThreshold = 200;
+ 
+# Filter by Area.
+params.filterByArea = True
+params.minArea = 4000
+ 
+# Filter by Circularity
+params.filterByCircularity = True
+params.minCircularity = 0.1
+ 
+# Filter by Convexity
+params.filterByConvexity = False
+params.minConvexity = 0.87
+ 
+# Filter by Inertia
+params.filterByInertia = False
+params.minInertiaRatio = 0.01
+
+im = cv2.imread('binder2.jpg', cv2.IMREAD_GRAYSCALE)
+detector = cv2.SimpleBlobDetector.create(params)
+
+keypoints = detector.detect(im)
+im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+cv2.imshow("Keypoints", im_with_keypoints)
+cv2.waitKey(0)
